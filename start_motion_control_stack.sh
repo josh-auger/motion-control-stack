095bb00a92a6c32312216ac9f38526ecab1d6ec5
jauger <joshua.auger@childrens.harvard.edu>
2026-04-29 13:30:56 -0400

    Specify user config settings for running motion-control-stack services

--- start_motion_control_stack.sh
+++ start_motion_control_stack.sh
@@ -18,7 +18,7 @@ MOCO_FLAG="off"
 # Registration type (by "slice", "smsgroup", or "volume") determines the grouping of image data
 REG_TYPE="smsgroup"
 # Toggles processing data sequentially ("first-in-first-out" = "on") or only the most recent data (FIFO = "off")
-FIFO_FLAG="on"
+FIFO_FLAG="off"
 # Head radius assumption (mm) for displacement calculations
 HEAD_RADIUS=50
 # Threshold for framewise displacement (mm) to flag motion
@@ -28,10 +28,10 @@ docker run --rm -it \
   -u $(id -u):$(id -g) \
   -p 9002:9002 \
   -p 8080:8080 \
-  -v $(pwd)/data:/data \
-  -e MOCO_FLAG=off \
-  -e FIFO_FLAG=on \
-  -e REG_TYPE=smsgroup \
-  -e HEAD_RADIUS=50 \
-  -e MOTION_THRESH=0.3 \
+  -v $DATA_DIR:/data \
+  -e MOCO_FLAG="$MOCO_FLAG" \
+  -e FIFO_FLAG="$FIFO_FLAG" \
+  -e REG_TYPE="$REG_TYPE" \
+  -e HEAD_RADIUS="$HEAD_RADIUS" \
+  -e MOTION_THRESH="$MOTION_THRESH" \
   jauger/motion-control-stack:dev all
