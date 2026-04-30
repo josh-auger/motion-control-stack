3fc71e46f85ea1b74325b2457a64679a44a7fdaf
jauger <joshua.auger@childrens.harvard.edu>
2026-04-30 13:21:09 -0400

    Include build command for shared-base-build image

--- build_motion_control_stack.sh
+++ build_motion_control_stack.sh
@@ -1,3 +1,6 @@
 
-docker build --no-cache -t jauger/motion-control-stack:dev -f ./docker/Dockerfile.unified .
-#docker build --rm -t jauger/motion-control-stack:dev -f ./docker/Dockerfile.unified .
+# REQUIREMENT: shared-base-build image is a pre-requisite for the motion-control-stack container build
+#docker build --no-cache -t shared-base-build:latest -f ./docker/Dockerfile.base .
+
+#docker build --no-cache -t jauger/motion-control-stack:dev -f ./docker/Dockerfile.unified .
+docker build --rm -t jauger/motion-control-stack:dev -f ./docker/Dockerfile.unified .
