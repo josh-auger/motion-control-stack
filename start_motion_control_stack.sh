d01d33693e556e328e509e16027d039aa6da7b40
jauger <joshua.auger@childrens.harvard.edu>
2026-06-08 12:02:18 -0400

    Format bash run script to start all motion control services

--- start_motion_control_stack.sh
+++ start_motion_control_stack.sh
@@ -1,5 +1,5 @@
 # SIMULTANEOUS MULTI-SERVICE RUN
-# ================================
+# =====================================
 # Bash script to start stack of motion monitoring and control services ("motion_control_stack"):
 #   fire-server = python fire server for image data send/receive with scanner
 #   queue-processor = execute image registration of compiled image data
@@ -9,21 +9,29 @@
 #   sh start_motion_control_stack.sh
 
 
-# User settings
-# ================================
+# User-defined Configuration Parameters
+# =====================================
 # Local host directory for output files
 DATA_DIR=$(pwd)"/data"
+
 # Moco flag ("on", "off") toggles sending moco feedback to the scanner for prospective motion correction
 MOCO_FLAG="off"
+
 # Registration type (by "slice", "smsgroup", or "volume") determines the grouping of image data
 REG_TYPE="smsgroup"
+
 # Toggles processing data sequentially ("first-in-first-out" = "on") or only the most recent data (FIFO = "off")
 FIFO_FLAG="off"
+
 # Head radius assumption (mm) for displacement calculations
 HEAD_RADIUS=50
+
 # Threshold for framewise displacement (mm) to flag motion
 MOTION_THRESH=0.3
 
+
+# Docker Run Command
+# =====================================
 docker run --rm -it \
   -u $(id -u):$(id -g) \
   -p 9002:9002 \
