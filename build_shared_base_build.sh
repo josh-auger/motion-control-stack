42161ba31f32e4582b6a26d3b71ef49ed98c5fb3
Josh A <jauger@bu.edu>
2026-07-31 11:30:18 -0400

    Initial refactor attempt of dockerfile.base, split into devel and runtime targets

--- build_shared_base_build.sh
+++ build_shared_base_build.sh
@@ -1,1 +1,4 @@
-docker build --no-cache -t shared-base-build:latest -f ./docker/Dockerfile.base .
+# docker build --no-cache -t shared-base-build:latest -f ./docker/Dockerfile.base .
+
+docker build --target build-base   -t shared-base-build:devel   -f dockerfile.base .
+docker build --target runtime-base -t shared-base-build:runtime -f dockerfile.base .
