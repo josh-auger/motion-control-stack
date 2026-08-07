18c8179d3ec7e1e69ff9c57c6f5bcaefc95e25ba
jauger <joshua.auger@childrens.harvard.edu>
2026-08-07 13:50:58 -0400

    optimize docker SLIMM build context

--- build_motion_control_stack.sh
+++ build_motion_control_stack.sh
@@ -4,12 +4,9 @@
 
 # docker build --no-cache -t jauger/motion-control-stack:dev -f ./docker/Dockerfile.unified .
 
-# Temporarily stage SLIMM-v3 for Docker build
-rm -rf external/SLIMM-v3
-cp -r ../SLIMM-v3 external/SLIMM-v3
-
 # Build the motion-control-stack container with CUDA support for SLIMM-v3
-docker build -t jauger/motion-control-stack:cuda -f ./docker/Dockerfile.unified .
-
-# Remove staged copy
-rm -rf external/SLIMM-v3
+docker build \
+    --build-context slimm=../SLIMM-v3 \
+    -t jauger/motion-control-stack:cuda \
+    -f ./docker/Dockerfile.unified \
+    .
